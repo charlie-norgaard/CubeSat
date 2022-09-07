b153023e0638f88f16f247dfb17d88e155b752ab
Charlie Norgaard <67657420+charlie-norgaard@users.noreply.github.com>
2022-09-07 15:28:04 -0700

    Add files via upload

--- pySat/soci.py
+++ pySat/soci.py
@@ -1,36 +1,35 @@
 import serial
 import telemetry_packets as tlm
 import command_packets as cmd
-import communication_protocols as protocol
 
 def get_cmds():
 
     # Initialize variables
-    serial_port = serial.Serial(port='COM11', baudrate=9600)
+    serial_port = serial.Serial(port='COM2', baudrate=9600, timeout=None)
     payload_size_data = serial_port.read(4)
     payload_size = int.from_bytes(payload_size_data, 'big') // 8
-    payload = serial_port.read(payload_size - 4)
+    payload = serial_port.read_until(size=payload_size+4)
     print(f'Total Payload Size: {payload_size} bytes')
+
 
     # Return the data
     print('\n--- Start Payload ---\n')
     print(payload)
     print('\n--- End Payload ---\n')
     return payload, payload_size_data
-    
+
 
 # UPDATE: Trying TERMINATED Protocol, delineate packets on termination character
 def uplink():
 
-    # Init variables
-    term_char = b'\x0A'
-
     # Get the data from the serial port
     print('\n* GRABBING DATA *\n')
     payload, payload_size = get_cmds()
+    nack_array = [0, 0, 0, 0]
 
     # Parse the data into packets
     print('* PROCESSING PACKETS *\n')
+    term_char = b'\xab\xcd'
     packets = payload.split(term_char)
     for i in range(len(packets)):
         print(f'Packet {i}:', packets[i], '\n')
@@ -39,30 +38,40 @@ def uplink():
     print('----- PAYLOAD CONTENTS -----\n')
     for packet in packets:
         pkt_id = int.from_bytes(packet[0:4], 'big')
-        if pkt_id == 0:
-            print('Beacon')
-        elif pkt_id == 1:
-            cmd.header(packet, payload_size)
+        if pkt_id == 1:
+            header_ack = cmd.header(packet, payload_size) # boolean
+            if header_ack == False:
+                nack_array[pkt_id - 1] = 1
+            print('--------------------')
         elif pkt_id == 2:
-            cmd.com(packet)
+            com_ack = cmd.com(packet) # boolean
+            if com_ack == False:
+                nack_array[pkt_id - 1] = 1
+            print('--------------------')
         elif pkt_id == 3:
-            print('GNC')
+            gnc_ack = cmd.gnc(packet) # boolean
+            if gnc_ack == False:
+                nack_array[pkt_id - 1] = 1
+            print('--------------------')
         elif pkt_id == 4:
-            print('SOAR')
-        elif pkt_id == 5:
-            print('IMG')
+            img_ack = cmd.img(packet) # boolean
+            if img_ack == False:
+                nack_array[pkt_id - 1] = 1
+            print('--------------------')
 
-    # cmd.header(packets[0], payload_size)
-    # cmd.com(packets[1])
+    print('\n* NACK PROTOCOL *\n')
+    print(f' nack_array = {nack_array}')
+
+    # How and where should I store / return the nack array
+    
 
 
 def downlink():
 
     # Serial Port
-    ser = serial.Serial('COM11')
+    ser = serial.Serial('COM1')
     
     # Downlink Payload
-    
     beacon = tlm.beacon()
     header = tlm.header()
     eps = tlm.eps()
@@ -72,7 +81,6 @@ def downlink():
     # img = tlm.img()
     
     # Send the packets
-
     # ser.write(beacon)
     ser.write(header)
     ser.write(eps)
